53f87a67c5d37ea51b43856f4978cb5dec02473f
José Viejo HUerta <Jviejo@users.noreply.github.com>
2024-01-13 16:00:50 +0100

    dirname

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require('express');
 
 const https = require('https');
 const fs = require('fs');
+const path = require('path');
 const app = express();
 
 https.createServer({
-    cert: fs.readFileSync('cert.pem'),
-    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync(path.join(__dirname,  'cert.pem')),
+    key: fs.readFileSync(path.join(__dirname, 'key.pem')),
     requestCert: true,
     rejectUnauthorized: false,
-    ca: fs.readFileSync('cert.pem')
+    ca: fs.readFileSync(path.join(__dirname, 'cert.pem'))
 }, app).listen(3333, function () {
     console.log('Servidor https correindo en el puerto 3333');
 });
